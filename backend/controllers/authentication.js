ce832aed7c9de07e34ff07fad0697886d3e6455e
robertnelson-co <hello@robertnelson.co>
2024-05-08 19:48:42 -0400

    create JWT on login

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -13,7 +14,8 @@ router.post('/', async (req, res) => {
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userid })
+        res.json({ user: user, token: result.value })
     }
 })
 
