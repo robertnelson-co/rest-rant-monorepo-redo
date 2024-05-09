9808528244be9316a3905f2b76a61a8d95a839b5
robertnelson-co <hello@robertnelson.co>
2024-05-08 20:28:07 -0400

    hide buttons from non admins

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -20,6 +20,21 @@ function PlaceDetails() {
 		fetchData()
 	}, [placeId])
 
+	let placeActions = null
+
+if (currentUser?.role === 'admin') {
+    placeActions = (
+        <>
+            <a className="btn btn-warning" onClick={editPlace}>
+                Edit
+            </a>
+            <button type="submit" className="btn btn-danger" onClick={deletePlace}>
+                Delete
+            </button>
+        </>
+    )
+}
+
 	if (place === null) {
 		return <h1>Loading</h1>
 	}
@@ -128,6 +143,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
+					{placeActions}
 					<a className="btn btn-warning" onClick={editPlace}>
 						Edit
 					</a>{` `}
